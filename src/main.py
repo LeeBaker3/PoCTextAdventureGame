469897deb07930c9cf63f6e25f9debd98a8368e5
LeeBaker3 <LeeBaker3@users.noreply.github.com>
2023-07-04 10:04:39 +1000

    Location and Item XML Updates
    Updated locations.xml with additional locations and removed items
    details into a separate xml file.

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
 #import player
 #import location
 from player import Player
-from Code.location import Location
-from Code.location import LoadLocations 
+from src.location import Location
+from src.location import LoadLocations 
 
 welcome = 'Welcome to your greatest adventure'
 introduction = 'We are going on an adventure. But first, make sure your parents know {0}! Remember, never go on adventures with strangers'
