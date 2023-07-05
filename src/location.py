135ef13dda146e4cc807855d6393ba5c92ec23e7
Lee Baker <lee.baker1@ymail.com>
2023-07-05 19:42:31 +1000

    Tidying up and sync repo

--- src/location.py
+++ src/location.py
@@ -1,22 +1,19 @@
 class Location:
-    
+
     def __init__(self, name, description, possibleMoves):
         self.name = name
         self.description = description
         self.items = [None]
-        self.possibeMoves = [None]
+        self.possibleMoves = [None]
+
 
 class LoadLocations:
-    
+
     def __init__(self, locations):
         self.locations = locations
-        
+
     def load(self):
         pass
-    
+
     def locations(self):
         return self.locations
-    
-    
-    
-    
