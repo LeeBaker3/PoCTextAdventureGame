76a1abaa4c0bd2443b7774ee4d48747b30bc6302
LeeBaker3 <LeeBaker3@users.noreply.github.com>
2023-10-26 13:29:10 +1000

    Added OpenAI Function
    Added a prototype OpenAI function to determine the action of the user
    from the user input

--- src/main.py
+++ src/main.py
@@ -1,10 +1,17 @@
-# import player
-# import location
+#import player
+#import location
+
 from player import Player
-from location import Location
-from location import LoadLocations
 from item import Item
 from item import LoadItems
+from locations import Location
+from locations import LoadLocations
+
+import os
+import openai
+#cwd = os.getcwd()
+#print("Current working directory:", cwd)
+
 
 locations = {}
 loadLocations = LoadLocations(locations, "locations.xml")
@@ -14,15 +21,18 @@ items = {}
 loadItems = LoadItems(items, "items.xml")
 loadItems.load()
 
+start = "\033[1m" #Bold text
+end = "\033[0;0m" #Normal text
+
 
 welcome = 'Welcome to your greatest adventure'
 introduction = 'We are going on an adventure. But first, make sure your parents know {0}! Remember, never go on adventures with strangers'
 
 print(welcome)
-playerName = input("What is your name adventurer?")
+playerName = input(start + "What is your name adventurer? :" + end)
 player = Player(playerName=playerName)
 
-print('Hello {0}'.format(player.name()))
+print('\nHello {0}'.format(player.name()))
 print(introduction.format(player.name()))
 
 
@@ -56,7 +66,25 @@ def itemsMessage(currentLocation, items):
     else:
         itemsMessage = 'There are {0} items here. ' + itemList
     print(itemsMessage.format(itemsLen))
+    
+# Define your OpenAI API key
+api_key = "YOUR_API_KEY"
 
+# Function to interact with ChatGPT and determine the move
+def determineUserInput(possibleActions, playerInput):
+    # Create a prompt that includes the possible moves and user input
+    prompt = f"Possible actions: {', '.join(possibleActions)}. User input: {userInput}"
+
+    # Get ChatGPT's response to determine the move
+    response = openai.Completion.create(
+        engine="text-davinci-002",  # You can choose another engine if needed
+        prompt=prompt,
+        max_tokens=1,  # Set to 1 to get a single word response
+        api_key=api_key
+    )
+
+    action = response.choices[0].text.strip()  # Get the move recommended by ChatGPT
+    return action
 
 currentLocation = locations['1']
 health = 100
@@ -66,3 +94,8 @@ health = 100
 print(currentLocation.location_description)
 movesMessage(currentLocation)
 itemsMessage(currentLocation, items)
+
+playerInput = input(start + "What would you like to do? :" + end)
+userAction = determineUserInput(currentLocation, playerInput)
+
+
