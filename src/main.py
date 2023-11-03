7632bb35c6e49d35bce19d9b0035c7ecd655fbaa
LeeBaker3 <LeeBaker3@users.noreply.github.com>
2023-11-04 09:35:41 +1000

    Correct ChatGPT API Integration
    Modified moveMessage functtion. ChatGPT API was using an old model that
    wouldn't comparisions

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
-#import player
-#import location
+# import player
+# import location
 
 from player import Player
 from item import Item
@@ -7,8 +7,10 @@ from item import LoadItems
 from locations import Location
 from locations import LoadLocations
 
+import config
 import os
 import openai
+
 #cwd = os.getcwd()
 #print("Current working directory:", cwd)
 
@@ -21,15 +23,15 @@ items = {}
 loadItems = LoadItems(items, "items.xml")
 loadItems.load()
 
-start = "\033[1m" #Bold text
-end = "\033[0;0m" #Normal text
+start = '\033[1m' #Bold text
+end = '\033[0;0m' #Normal text
 
 
 welcome = 'Welcome to your greatest adventure'
 introduction = 'We are going on an adventure. But first, make sure your parents know {0}! Remember, never go on adventures with strangers'
 
 print(welcome)
-playerName = input(start + "What is your name adventurer? :" + end)
+playerName = input('{}{}{}'.format(start, 'What is your name adventurer? :', end))
 player = Player(playerName=playerName)
 
 print('\nHello {0}'.format(player.name()))
@@ -37,55 +39,71 @@ print(introduction.format(player.name()))
 
 
 def movesMessage(currentLocation):
-    moves = currentLocation.movesLength()
-    if moves > 1:
-        moveMessage = 'There are {0} possible moves'
-    else:
-        moveMessage = 'There is {0} possible move'
-    print(moveMessage.format(moves))
-
-
+	moveslen = currentLocation.movesLength()
+	
+	currentLocation.possibleMoves.items()
+	moveList = ''
+	
+	for key, value in currentLocation.possibleMoves.items():
+		moveList = moveList + ' ' + value
+		
+		
+	print (moveList)
+	
+	if moveslen > 1:
+		moveMessage = 'There are {0} possible moves' + moveList
+	else:
+		moveMessage = 'There is {0} possible move' + moveList
+	print(moveMessage.format(moveslen))
+	
+	
 def itemsMessage(currentLocation, items):
 
-    itemsLen = currentLocation.itemsLength()
-
-    itemList = 'A '
-    if itemsLen == 1:
-        itemList = itemList + items[currentLocation.items[0]].item_name
-    else:
-        for item in enumerate(currentLocation.items):
-            if item in {0, itemsLen-1}:
-                itemList = 'and ' + items[str(item)].item_name
-            else:
-                itemList = ', ' + items[str(item)].item_name
-
-    if itemsLen == 0:
-        itemsMessage = 'There are no items here '
-    elif itemsLen == 1:
-        itemsMessage = 'There is {0} item here. ' + itemList
-    else:
-        itemsMessage = 'There are {0} items here. ' + itemList
-    print(itemsMessage.format(itemsLen))
-    
+	itemsLen = currentLocation.itemsLength()
+	
+	itemList = 'A '
+	if itemsLen == 1:
+		itemList = itemList + items[currentLocation.items[0]].item_name
+	else:
+		for item in enumerate(currentLocation.items):
+			if item in {0, itemsLen-1}:
+				itemList = 'and ' + items[str(item)].item_name
+			else:
+				itemList = ', ' + items[str(item)].item_name
+				
+	if itemsLen == 0:
+		itemsMessage = 'There are no items here '
+	elif itemsLen == 1:
+		itemsMessage = 'There is {0} item here. ' + itemList
+	else:
+		itemsMessage = 'There are {0} items here. ' + itemList
+	print(itemsMessage.format(itemsLen))
+	
 # Define your OpenAI API key
-api_key = "YOUR_API_KEY"
+api_key = config.openai['api_key']
+
+print('API key:' + api_key)
 
 # Function to interact with ChatGPT and determine the move
 def determineUserInput(possibleActions, playerInput):
-    # Create a prompt that includes the possible moves and user input
-    prompt = f"Possible actions: {', '.join(possibleActions)}. User input: {userInput}"
-
-    # Get ChatGPT's response to determine the move
-    response = openai.Completion.create(
-        engine="text-davinci-002",  # You can choose another engine if needed
-        prompt=prompt,
-        max_tokens=1,  # Set to 1 to get a single word response
-        api_key=api_key
-    )
-
-    action = response.choices[0].text.strip()  # Get the move recommended by ChatGPT
-    return action
-
+	# Create a prompt that includes the possible moves and user input
+	# Create a system message and a user message with possible moves and user input
+	messages = [
+	{"role": "system", "content": "You are a text-based adventure game character."},
+	{"role": "user", "content": f"Possible actions: {', '.join(possibleActions)}. User input: {playerInput}"}
+	]
+	print(messages)
+	# Get ChatGPT's response to determine the move
+	response = openai.ChatCompletion.create(
+	model="gpt-3.5-turbo",  # Use ChatGPT-3.5
+	messages=messages,
+	max_tokens=1,  # Set to 1 to get a single word response
+	api_key=api_key
+	)
+	
+	action = response.choices[0].message['content'].strip()  # Get the move recommended by ChatGPT-3.5
+	return action
+	
 currentLocation = locations['1']
 health = 100
 
@@ -96,6 +114,8 @@ movesMessage(currentLocation)
 itemsMessage(currentLocation, items)
 
 playerInput = input(start + "What would you like to do? :" + end)
-userAction = determineUserInput(currentLocation, playerInput)
-
+possibleMoves = currentLocation.possibleMoves.values()
+print(possibleMoves)
+userAction = determineUserInput(possibleMoves, playerInput)
+print ("You choose to: {}".format(userAction))
 
